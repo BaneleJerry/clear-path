28c2b42c472aa002665e1c2fcbf91aa23352ddd8
Banele Thabede <banelethabede@outlook.com>
2026-03-07 12:12:44 +0200

    init commit

---
+++ src/test/java/com/banelethabede/clear_path/ClearPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banelethabede.clear_path;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
