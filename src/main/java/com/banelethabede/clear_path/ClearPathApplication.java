1665e053720c14bbd912c6c0b13de280eaa2a7e3
BaneleJerry <banelethabede@outlook.com>
2026-03-08 20:22:17 +0200

    init commit

---
+++ src/main/java/com/banelethabede/clear_path/ClearPathApplication.java
@@ -0,0 +1,13 @@
+package com.banelethabede.clear_path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearPathApplication.class, args);
+	}
+
+}
