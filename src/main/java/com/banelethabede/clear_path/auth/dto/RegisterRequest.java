3f3291a4b5ecbbdae0121d87d80dddf30b92cedb
BaneleJerry <banelethabede@outlook.com>
2026-03-08 20:22:18 +0200

    modified auth dto

--- src/main/java/com/banelethabede/clear_path/auth/dto/RegisterRequest.java
+++ src/main/java/com/banelethabede/clear_path/auth/dto/RegisterRequest.java
@@ -1,6 +1,10 @@
 package com.banelethabede.clear_path.auth.dto;
 
+import com.banelethabede.clear_path.organization.dto.OrganizationEnums;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,12 +12,25 @@ import lombok.Setter;
 @Setter
 public class RegisterRequest {
 
-    @NotBlank
+    // --- User Details ---
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    // --- Organization Details ---
+    @NotBlank(message = "Organization name is required")
+    private String organizationName;
+
+    @NotNull(message = "Organization type is required")
+    private OrganizationEnums organizationType;
 }
-
-
