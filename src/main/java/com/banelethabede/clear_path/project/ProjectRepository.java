72fd1a10ae854ecfa62d9eb58e0ba69bd7224265
BaneleJerry <banelethabede@outlook.com>
2026-03-09 20:04:44 +0200

    feat(project): implement project CRUD operations and DTO refactor

--- src/main/java/com/banelethabede/clear_path/project/ProjectRepository.java
+++ src/main/java/com/banelethabede/clear_path/project/ProjectRepository.java
@@ -3,6 +3,7 @@ package com.banelethabede.clear_path.project;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.banelethabede.clear_path.project.dto.ProjectStatusEnum;
@@ -12,5 +13,8 @@ public interface ProjectRepository extends JpaRepository<Project, UUID> {
     List<Project> findAllByOrganizationId(UUID organizationId);
 
     List<Project> findAllByProjectStatusEnum(ProjectStatusEnum status);
+
+   @EntityGraph(attributePaths = {"organization"})
+    List<Project> findByOrganizationId(UUID organizationId);
     
 }
